390f97f0202d4ea4f836a4c4f9fcf8a20fa418f0
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-03-22 15:45:37 -0300

    Agregar funcion selectWinner
    -Se agrega la funcion selectWinner que selecciona cual es el ganador entre la
    maquina y el usuario, para simplificacion y flexibilidad en los resultados.
    -Se deja console.log para probar las salidas y los retornos de las
    funciones.

--- JavaScript.js
+++ JavaScript.js
@@ -5,6 +5,7 @@ function getComputerChoice(){
     let decision = "piedra|papel|tijera";
     const array = decision.split("|");
     let random = Math.floor(Math.random()*3);
+    console.log(array[random]);
     return array[random];
 }
 
@@ -12,5 +13,18 @@ function getHumanChoice(){
     let respuesta = prompt("Ingrese una opcion. (Piedra|Papel|Tijera)");
     return respuesta.toLowerCase();
 }
+/* 0 es para empate, 1 es para computadora y 2 es para humano*/
+function selectWinner(humanChoice, computerChoice){
+    let ganador = (humanChoice === computerChoice) ? 0 : (humanChoice === "piedra" && computerChoice === "papel") ? 1 : (humanChoice === "piedra" && computerChoice === "tijera") ? 2 
+    : (humanChoice === "papel" && computerChoice === "piedra") ? 2 : (humanChoice == "papel" && computerChoice === "tijera") ? 1 : (humanChoice === "tijera" && computerChoice === "piedra") ? 1 
+    : (humanChoice === "tijera" && computerChoice === "papel") ? 2 : -1;
+    return ganador;  
+}
+/*
+function playRound(humanChoice, computerChoice){
+    selectWinner(humanChoice,computerChoice);
 
-console.log(getHumanChoice());
+}
+*/
+ganador = selectWinner(getHumanChoice(),getComputerChoice());
+console.log(ganador);
