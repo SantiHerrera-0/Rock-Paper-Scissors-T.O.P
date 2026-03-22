abdaee081e3fa39bdab3898a3d322a2b1d5c2e48
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-03-22 16:16:56 -0300

    Agregar funcion de juego
    -Se agrega la funcion playRound para mostrar los mensajes
    correspondientes a cada situacion.

--- JavaScript.js
+++ JavaScript.js
@@ -5,7 +5,6 @@ function getComputerChoice(){
     let decision = "piedra|papel|tijera";
     const array = decision.split("|");
     let random = Math.floor(Math.random()*3);
-    console.log(array[random]);
     return array[random];
 }
 
@@ -13,18 +12,30 @@ function getHumanChoice(){
     let respuesta = prompt("Ingrese una opcion. (Piedra|Papel|Tijera)");
     return respuesta.toLowerCase();
 }
-/* 0 es para empate, 1 es para computadora y 2 es para humano*/
+/*Devuelve 0 para empate, 1 para computadora y 2 para humano*/
 function selectWinner(humanChoice, computerChoice){
     let ganador = (humanChoice === computerChoice) ? 0 : (humanChoice === "piedra" && computerChoice === "papel") ? 1 : (humanChoice === "piedra" && computerChoice === "tijera") ? 2 
     : (humanChoice === "papel" && computerChoice === "piedra") ? 2 : (humanChoice == "papel" && computerChoice === "tijera") ? 1 : (humanChoice === "tijera" && computerChoice === "piedra") ? 1 
     : (humanChoice === "tijera" && computerChoice === "papel") ? 2 : -1;
     return ganador;  
 }
-/*
-function playRound(humanChoice, computerChoice){
-    selectWinner(humanChoice,computerChoice);
 
-}
-*/
-ganador = selectWinner(getHumanChoice(),getComputerChoice());
-console.log(ganador);
+    function playRound(humanChoice, computerChoice){
+        ganador = selectWinner(humanChoice,computerChoice);
+        switch(ganador){
+            case 0:
+                console.log("Hubo empate!");
+                break;
+            case 1:
+                console.log(`Perdiste! ${computerChoice} vence a ${humanChoice}`);
+                computerScore ++;
+                break
+            case 2:
+                console.log(`Ganaste! ${humanChoice} vence a ${computerChoice}`);
+                humanScore ++;
+                break;
+            default:
+                console.log("No hubo un ingreso valido.");
+                break;
+        }
+    }
