8365c09b466c7be438235101f514c19dee4a285c
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-03-22 00:06:48 -0300

    Agregar decision de maquina
    -Se agrega la funcion que decide cual es la opcion que
    elige la maquina, se deja console.log para ver que opcion
    esta devolviendo.

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+function getComputerChoice(){
+    let decision = "Piedra|Papel|Tijera";
+    const array = decision.split("|");
+    let random = Math.floor(Math.random()*3);
+    return array[random];
+}
+console.log(getComputerChoice());
