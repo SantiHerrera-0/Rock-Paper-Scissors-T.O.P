5e6719f8aa14bfbc4921b4cc5223e7e4fccce557
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-03-22 00:34:55 -0300

    Agregar puntaje y decision de persona
    - Se agregaron las varibles scores de maquina y personas.
    Ademas de agrego la funcion que toma la decision de las
    personas y devuelve el valor en minusculas.
    -Se deja console.log para ver que valor devuelve la
    funcion getHumanChoice.

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,16 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    let decision = "Piedra|Papel|Tijera";
+    let decision = "piedra|papel|tijera";
     const array = decision.split("|");
     let random = Math.floor(Math.random()*3);
     return array[random];
 }
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let respuesta = prompt("Ingrese una opcion. (Piedra|Papel|Tijera)");
+    return respuesta.toLowerCase();
+}
+
+console.log(getHumanChoice());
