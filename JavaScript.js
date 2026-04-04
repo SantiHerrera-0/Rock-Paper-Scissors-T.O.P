0285e0c8341321d6bba42a5b02ba8ba205f26db9
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-04-04 12:56:29 -0300

    Agregar botones y eventListeners
    -Se añaden los botones y sus eventListeners
    en html para poder tener una UI mas apropiada
    y con posibilidad de jugar con el mouse y la
    ventana en donde se esta.
    -Se inhabilito funcion get human choice porque
    ya no es necesario que el usuario ingrese la
    opcion que quiere elegir.
    -Ciclo para jugar 5 rondas tambien inhabilitado
    por el momento para hacer partidas singulares
    por el momento.

--- JavaScript.js
+++ JavaScript.js
@@ -8,11 +8,12 @@ function getComputerChoice(){
     return array[random];
 }
 
-function getHumanChoice(){
+/*function getHumanChoice(){
     let respuesta = prompt("Ingrese una opcion. (Piedra|Papel|Tijera)");
     return respuesta.toLowerCase();
-}
+}*/
 /*Devuelve 0 para empate, 1 para computadora y 2 para humano*/
+
 function selectWinner(humanChoice, computerChoice){
     let ganador = (humanChoice === computerChoice) ? 0 : (humanChoice === "piedra" && computerChoice === "papel") ? 1 : (humanChoice === "piedra" && computerChoice === "tijera") ? 2 
     : (humanChoice === "papel" && computerChoice === "piedra") ? 2 : (humanChoice == "papel" && computerChoice === "tijera") ? 1 : (humanChoice === "tijera" && computerChoice === "piedra") ? 1 
@@ -41,18 +42,19 @@ function playRound(humanChoice, computerChoice){
 }
 
 function ganadorFinal(){
-    if (humanScore >computerScore){
+    if (humanScore > computerScore){
         console.log("El ganador es el humano!");
-    }else if(computerScore >humanScore){
+    }else if(computerScore > humanScore){
         console.log("La ganadora es la maquina!");
     }else{
         console.log("No hubo gandor!")
     }
-    
-}
-for(let i=0; i<5; i++){
-    playRound(getHumanChoice(), getComputerChoice());
-    console.log(`El puntaje es: Humano ${humanScore}, Maquina ${computerScore}`);
 }
 
-ganadorFinal();
+/*for(let i=0; i<5; i++){
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`El puntaje es: Humano ${humanScore}, Maquina ${computerScore}`);
+}*/
+
+
+
